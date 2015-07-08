1213fb9a383e0fd988b3e545fb6c80a760cb7cec
Jan Miksovsky <jan@quickui.org>
2015-07-07 17:13:49 -0700

    Add simple state.

--- client/spike.js
+++ client/spike.js
@@ -2,10 +2,18 @@ var Greet = React.createClass({
 
   displayName: 'Greet',
 
+  getInitialState: function() {
+    return {
+      punctuation: '.'
+    };
+  },
+
   render: function() {
     return (
       React.createElement('div', { className: 'test' },
-        "Hello, " + this.props.message + "."
+        "Hello, ",
+        this.props.message,
+        this.state.punctuation
       )
     );
   }
