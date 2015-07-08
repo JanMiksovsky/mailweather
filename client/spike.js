a4edcda71919c44fbdb818f86ae7b296bcfc0dbc
Jan Miksovsky <jan@quickui.org>
2015-07-08 16:48:53 -0700

    Change form to React class.

--- client/spike.js
+++ client/spike.js
@@ -2,29 +2,87 @@ var Greet = React.createClass({
 
   displayName: 'Greet',
 
-  getInitialState: function() {
-    return {
-      punctuation: '.'
-    };
-  },
-
   render: function() {
     return (
       React.createElement('div', { className: 'test' },
         "Hello, ",
         this.props.message,
-        this.state.punctuation
+        this.props.punctuation
       )
     );
   }
 
 });
 
+
+var MessageForm = React.createClass({
+
+  displayName: 'MessageForm',
+
+  render: function() {
+    return React.createElement(
+      'form',
+      {
+        action: '/message',
+        method: 'POST'
+      },
+      React.createElement(
+        'p',
+        null,
+        "From: ",
+        React.createElement('input', {
+          id: 'from',
+          type: 'text',
+          name: 'from',
+          value: this.props.from
+        })
+      ),
+      React.createElement(
+        'textarea',
+        {
+          type: 'text',
+          name: 'plain'
+        },
+        this.props.body
+      ),
+      React.createElement(
+        'p',
+        null,
+        React.createElement('input', {
+          type: 'submit',
+          value: 'Send'
+        })
+      )
+    );
+  }
+
+});
+
+
+var WeatherApp = React.createClass({
+
+  displayName: 'WeatherApp',
+
+  getInitialState: function() {
+    return {
+      from: 'jan@miksovsky.com',
+      body: '47.6329,-122.2800'
+    };
+  },
+
+  render: function() {
+    return React.createElement(MessageForm, {
+      from: this.state.from,
+      body: this.state.body
+    });
+  }
+
+});
+
+
 window.addEventListener('load', function() {
-  window.greet = React.render(
-    React.createElement(Greet, {
-      message: 'world'
-    }),
-    document.querySelector('#greet')
+  window.app = React.render(
+    React.createElement(WeatherApp),
+    document.querySelector('#app')
   );
 });
