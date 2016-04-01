5cfa7a22f76e184172a6d6e6a2ce3b56fd5996d6
Jan Miksovsky <jan@quickui.org>
2016-03-31 17:54:45 -0700

    Lint fix

--- test/forecastIo.js
+++ test/forecastIo.js
@@ -26,11 +26,11 @@ describe('ForecastIO', () => {
   it("calls forecast.io result", done => {
     sample.getForecast()
     .then(data => {
-      let scope = nock('https://api.forecast.io')
-          // Ignore API key for test purposes
-          .filteringPath(/\/forecast\/[a-z0-9]+/, '/forecast/KEY')
-          .get('/forecast/KEY/47.633082,-122.280192')
-          .reply(200, data);
+      nock('https://api.forecast.io')
+        // Ignore API key for test purposes
+        .filteringPath(/\/forecast\/[a-z0-9]+/, '/forecast/KEY')
+        .get('/forecast/KEY/47.633082,-122.280192')
+        .reply(200, data);
       let location = {
         "latitude": 47.633082,
         "longitude": -122.280192,
