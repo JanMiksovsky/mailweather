1f345403190c9e8bb2770b89b4f1eb6c6406106b
Jan Miksovsky <jan@quickui.org>
2016-05-24 21:44:39 -0700

    Try adjusting server date.

--- test.js
+++ test.js
@@ -1,2 +1,20 @@
-var d = new Date(1464149256 * 1000);
-console.log(d);
+'use strict';
+
+let forecastTime = 1464149256;
+let forecastMilliseconds = forecastTime * 1000;
+
+let date = new Date(forecastMilliseconds);
+console.log(`Raw date: ${date}`);
+
+let serverOffsetMinutes = date.getTimezoneOffset();
+console.log(`Timezone offset: ${serverOffsetMinutes}`);
+const MILLISECONDS_PER_MINUTE = 60 * 1000;
+const MILLISECONDS_PER_HOUR = 60 * 60 * 1000;
+let serverOffsetMilliseconds = serverOffsetMinutes * MILLISECONDS_PER_MINUTE;
+
+let forecastOffsetHours = -7;
+let forecastOffsetMilliseconds = forecastOffsetHours * MILLISECONDS_PER_HOUR;
+
+let adjustedMilliseconds = forecastMilliseconds + serverOffsetMilliseconds + forecastOffsetMilliseconds;
+let adjustedDate = new Date(adjustedMilliseconds);
+console.log(`Adjusted date: ${adjustedDate}`);
